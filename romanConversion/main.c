c21334afd064ba39b02eaabe47c85858ec2aa3d9
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-04 02:16:25 +0530

    some changes

--- romanConversion/main.c
+++ romanConversion/main.c
@@ -32,134 +32,153 @@ int main()
 		temp++;
 	}
 	
-	printf("size of your input is : %d\n", size);
+	//printf("size of your input is : %d\n", size);
 	
 	temp = first;
 	letterPtr = fix_letter;
 
-		// for(int i=0; i<size;i++)
-		// {
-		// 	for(int j=0;j < 7;j++)
-		// 	{
-		// 		if(fix_letter[j] == input[i])
-		// 		{
-		// 			switch (input[i])
-		// 			{
-
-		// 			}
-		// 			flag=0;
-		// 			//printf("%c ",input[i]);
-		// 			break;
-		// 		}
-		// 		else
-		// 		{
-		// 			flag=1;
-		// 		}
-		// 	}	
-		// }
-		///*********************************
-		for(i=0;i<size;i++)
+		for(int i=0; i<size;i++)
+		{
+			for(int j=0;j < 7;j++)
 			{
-        		input_char_by_char = input[i];
-        
-				switch(input_char_by_char)
+				if(fix_letter[j] == input[i])
 				{
-					case 'I':
+					switch (input[i])
 					{
-						count_I++;
-						sum+=1;
-						fix_roman_no[6]=1;
-						break;
-					}
-					case 'V':
-					{
-						count_V++;
-					if(fix_roman_no[6]>=5){
-						sum+=5;
-						fix_roman_no[6]=5;
-					}
-					else{
-						sum=5-sum;
-						
-					}
-					break;
-					}
-					case 'X':
-					{
-						 count_X++;
-					if(fix_roman_no[6]>=10){
-						sum+=10;
-						fix_roman_no[6]=10;
-					}
-					else{
-						sum=10-sum;
-					}
-					break;
-					}
-					case 'L':
-					{
-						count_L++;
-					if(fix_roman_no[6]>=50){
-						sum+=50;
-						fix_roman_no[6]=50;
-					}
-					else{
-						sum=50-sum;
-					}
-					break;
-					}
-					case 'C':
-					{
-						 count_C++;
-					if(fix_roman_no[6]>=100){
-						sum+=100;
-						fix_roman_no[6]=100;
-					}
-					else{
-						sum=100-sum;
-					}
-					break;
-					}
-					case 'D':
-					{
-						count_D++;
-					if(fix_roman_no[6]>=500){
-						sum+=500;
-						fix_roman_no[6]=500;
-					}
-					else{
-						sum=500-sum;
-					}
-					break;
-					}
-					case 'M':
-					{
-					if(fix_roman_no[6]>=1000){
-						sum+=1000;
-						fix_roman_no[6]=1000;
-					}
-					else{
-						sum=1000-sum;
-					}
-					break;
-					}
-					default :
-					{
-						printf("\nInvalid entry\n");
-						break;
-					}
 
+					}
+					flag=0;
+					//printf("%c ",input[i]);
+					break;
 				}
-		    }
-			if(( count_I <= 3 ) && ( count_C <= 3 ) && ( count_X <=3 )&&(count_V<=1)&&(count_L<=1)&&(count_D<=1))
-			{
-				printf("\nInterger is : %d\n",sum);
-			}
-			else
-			{
-				printf("\ninvalid input\n");
-			}
-		    
+				else
+				{
+					flag=1;
+				}
+			}	
+		}
+		if (flag == 0 )
+		{
+			for(i=0;i<size;i++)
+				{
+					input_char_by_char = input[i];
+			
+					switch(input_char_by_char)
+					{
+						case 'I':
+						{
+							count_I++;
+							sum+=1;
+							fix_roman_no[6]=1;
+							//printf("\nvalue of fix_roman_no :%d\n",fix_roman_no[6]);
+							break;
+						}
+						case 'V':
+						{
+							count_V++;
+
+							if(fix_roman_no[6]>=5)
+							{
+								sum+=5;
+								fix_roman_no[6]=5;
+							}
+							else if(sum == 1){
+								sum=5-sum;	
+							}
+							else
+							{
+								count_I=count_I+2;
+							}
+
+						break;
+						}
+						case 'X':
+						{
+							count_X++;
+							if(fix_roman_no[6]>=10)
+							{
+								sum+=10;
+								fix_roman_no[6]=10;
+							}
+							else if(sum == 1)
+							{
+								sum=10-sum;
+							}
+							else
+							{
+								count_X=count_X+2;
+							}
+
+						break;
+						}
+						case 'L':
+						{
+							count_L++;
+						if(fix_roman_no[6]>=50){
+							sum+=50;
+							fix_roman_no[6]=50;
+						}
+						else{
+							sum=50-sum;
+						}
+						break;
+						}
+						case 'C':
+						{
+							count_C++;
+						if(fix_roman_no[6]>=100){
+							sum+=100;
+							fix_roman_no[6]=100;
+						}
+						else{
+							sum=100-sum;
+						}
+						break;
+						}
+						case 'D':
+						{
+							count_D++;
+						if(fix_roman_no[6]>=500){
+							sum+=500;
+							fix_roman_no[6]=500;
+						}
+						else{
+							sum=500-sum;
+						}
+						break;
+						}
+						case 'M':
+						{
+						if(fix_roman_no[6]>=1000){
+							sum+=1000;
+							fix_roman_no[6]=1000;
+						}
+						else{
+							sum=1000-sum;
+						}
+						break;
+						}
+						default :
+						{
+							printf("\nInvalid entry\n");
+							break;
+						}
+
+					}
+				}
+				if(( count_I <= 3 ) && ( count_C <= 3 ) && ( count_X <=3 )&&(count_V<=1)&&(count_L<=1)&&(count_D<=1))
+				{
+					printf("\nInterger is : %d\n",sum);
+				}
+				else
+				{
+					printf("\ninvalid input\n");
+				}
+		}
+		else
+		{
+			printf("\ninvalid input\n");
+		}
 
 		///***********************************
 		//i = 0;
