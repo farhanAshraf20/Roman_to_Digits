60a4bd6d0244f46aa8cedd752c3a4c4ad28047f0
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-04 12:58:15 +0530

    some changes

--- romanConversion/main.c
+++ romanConversion/main.c
@@ -1,70 +1,82 @@
-#include"includes.h"
-//char *far;
-//char input_char_by_char;
-//int size=0;
-// int i=0;
- int flag =1;
-// int sum = 0;
-// int count_I=0, count_X=0, count_C=0,count_V=0,count_L=0,count_D=0;
+#include "headers.h"
+
+//define global vars -- declared in headers.h
+int flag = 1;
+int size = 0;
+char fix_letter[]="IVXLCDM";
+int fix_roman_no[] = {1,5,10,50,100,500,1000};
+
+
 
 int main()
 {
-	char *input;
-	char *temp, *first;
-	//char *letterPtr;
-	
-	printf("Enter a roman No. (b/w I to M) : ");
-	scanf("%s", input);
+    char *input;
+    char *temp, *first;
 
-	first = input;
-	far=input;
-	temp = first;
+    //char *letterPtr;
 
-	// printf("your input : %s", input);
-	
-	// printf("\n%s\n",temp);
-	
-	// temp = first;
-	
-	
-	while(*temp != '\0')
-	{
-		size += sizeof(*temp);
-		temp++;
-	}
-	
-	//printf("size of your input is : %d\n", size);
-	
-	//temp = first;
+    printf("Enter a roman No. (b/w I to M) : ");
+    scanf("%s", input);
 
-		for(int i=0; i<size;i++)
-		{
-			for(int j=0;j < 7;j++)
-			{
-				//if(fix_letter[j] == input[i])
-				if(fix_letter[j] == input[i])
-				{
-					flag=0;
-					//printf("%c ",input[i]);
-					break;
-				}
-				else
-				{
-					flag=1;
-				}
-			}	
-		}
-		if (flag == 0 )
-		{
-			function_check();
-		}
-		else
-		{
-			//printf("\nINPUT !!!!! : %s\n",first);
-			printf("\ninvalid input\n");
-		}
+    first = input;
+    far=input;
+    temp = first;
 
-		///***********************************
+    // printf("your input : %s", input);
 
-	return 0;
+    // printf("\n%s\n",temp);
+
+    // temp = first;
+
+
+    while(*temp != '\0')
+    {
+        size += sizeof(*temp);
+        temp++;
+    }
+
+    //printf("size of your input is : %d\n", size);
+
+    //temp = first;
+
+        for(int i=0; i<size;i++)
+        {
+            for(int j=0;j < 7;j++)
+            {
+                //if(fix_letter[j] == input[i])
+                if(fix_letter[j] == input[i])
+                {
+                    flag=0;
+                    //printf("%c ",input[i]);
+                    break;
+                }
+                else
+                {
+                    flag=1;
+                }
+            }    
+        }
+        if (flag == 0 )
+        {
+            check_num();
+
+            if(( count_I <= 3 ) && ( count_C <= 3 ) && ( count_X <=3 )&&(count_V<=1)&&(count_L<=1)&&(count_D<=1))
+            {
+                printf("\nInterger is : %d\n",sum);
+            }
+            else
+            {
+                printf("\ninvalid input\n");
+            }
+        }
+
+        else
+        {
+            //printf("\nINPUT !!!!! : %s\n",first);
+            printf("\ninvalid input\n");
+        }
+
+        ///***********************************
+
+    return 0;
 }
