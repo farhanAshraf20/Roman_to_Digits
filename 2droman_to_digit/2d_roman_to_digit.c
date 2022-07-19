deeec17cad02f481890379fefb61c9968a4ecfdc
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-19 10:53:32 +0530

    some update

--- 2droman_to_digit/2d_roman_to_digit.c
+++ 2droman_to_digit/2d_roman_to_digit.c
@@ -3,15 +3,17 @@
 int main(){      
 int i=0,j=0;   
 char roman[4];
+char arr[10][5]={"I","II","III","IV","V","VI","VII","VIII","IX","X"};  
 printf("Please give input I to X : ");
 scanf("%s",roman);
 //printf("\n roman = %s\n",roman);
-char arr[10][5]={"I","II","III","IV","V","VI","VII","VIII","IX","X"};  
 for(i=0;i<10;i++){   
-    char *temp;
-    temp = arr[i];
+    //char *temp;
+    //temp = arr[i];
+    char temp[5];
+    strcpy(temp , arr[i]);
    // printf("Output : %s\n",temp);
-    if(strcmp(roman, temp)==0)
+    if(strcmp(roman, temp) == 0)
     {
        printf("OUTPUT : %s = %d\n",arr[i],(i+1));
     }
