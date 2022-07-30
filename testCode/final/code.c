fee7ab0419a3554a045e0a6f12fdbef06efdde91
Farhan Ashraf <98956649+farhanAshraf20@users.noreply.github.com>
2022-07-30 15:24:37 +0530

    Update code.c

--- testCode/final/code.c
+++ testCode/final/code.c
@@ -1,3 +1,9 @@
+/*
+Roman to digit code 
+I to M (1 to 1000)
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,19 +13,13 @@
 
 char fix_letter[] = "IVXLCDM";
 char *fun_input;
-//int flag = 1;
-int max = 1000;
-int size = 0;
-int sum = 0;
 
-int count_I = 0, count_X = 0, count_C = 0, count_V = 0, count_L = 0, count_D = 0;
 
 int input_check();
-
 int function_check(char *fun_input);
 
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[])					// main function
 {
 
 	//printf("No of argument : %d \n",argc);
@@ -28,54 +28,31 @@ int main(int argc, char* argv[])
 	printf("Argument No2 :%s \n",*argv);
 	
 	fun_input = *argv;
-    	/*
-    	input = *argv;
-    	fun_input = input;
     	
-	size = strlen(fun_input);
-    
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            if (fix_letter[j] == input[i])
-            {
-                flag = 0;
-                // printf("%c ",input[i]);
-                break;
-            }
-            else
-            {
-                flag = 1;
-            }
-        }
-    }
-    
-    */
-    if (input_check() == 0)
-    {
-    	printf("\n-------------------------------------------------------\n");
-        function_check(fun_input);
-    }
-    else
-    {
-        // printf("\nINPUT !!!!! : %s\n",first);
-        printf("\ninvalid input\n");
-    }
+	if (input_check() == 0)
+	{
+		printf("\n-------------------------------------------------------\n");
+		function_check(fun_input);
+	}
+	else
+	{
+		exit(0);
+	}
 
-    ///***********************************
 
     return 0;
 }
 
 
-int input_check()
+int input_check()							//check input function
 {	
 
 	char *input;
-	input = fun_input ;
-    	
+	int size = 0;
 	int flag = 1;
+	
+	
+	input = fun_input ;
     	
 	size = strlen(input);
     
@@ -95,6 +72,8 @@ int input_check()
 			}
 		}
 	}
+	
+	
 	if(flag == 0)
 	{
 		return 0;
@@ -109,140 +88,144 @@ int input_check()
 
 
 
-int function_check(char *fun_input)
+int function_check(char *fun_input)					// code locic function
 {
 
-    static int i=0;
+	static int i=0;							//for use input Traverse 
+	int max = 1000;							
+	int sum = 0;
+
+	int count_I = 0, count_X = 0, count_C = 0, count_V = 0, count_L = 0, count_D = 0;
   
-    switch (fun_input[i])
-    {
-        case 'I':
-        {
-            count_I++;
-            sum += 1;
-            max = 1;
-            // printf("\nvalue of max :%d\n",max);
-            break;
-        }
-        case 'V':
-        {
-            count_V++;
+	switch (fun_input[i])
+	{
+		case 'I':
+		{
+		    count_I++;
+		    sum += 1;
+		    max = 1;
+		    // printf("\nvalue of max :%d\n",max);
+		    break;
+		}
+		case 'V':
+		{
+		    count_V++;
 
-            if (max >= 5)
-            {
-                sum += 5;
-                max = 5;
-            }
-            else if (sum == 1)
-            {
-                sum = 5 - sum;
-            }
-            else
-            {
-                count_I = count_I + 2;
-            }
+		    if (max >= 5)
+		    {
+			sum += 5;
+			max = 5;
+		    }
+		    else if (sum == 1)
+		    {
+			sum = 5 - sum;
+		    }
+		    else
+		    {
+			count_I = count_I + 2;
+		    }
 
-            break;
-        }
-        case 'X':
-        {
-            count_X++;
-            if (max >= 10)
-            {
-                sum += 10;
-                max = 10;
-            }
-            else if (sum == 1)
-            {
-                sum = 10 - sum;
-            }
-            else
-            {
-                count_X = count_X + 2;
-            }
+		    break;
+		}
+		case 'X':
+		{
+		    count_X++;
+		    if (max >= 10)
+		    {
+			sum += 10;
+			max = 10;
+		    }
+		    else if (sum == 1)
+		    {
+			sum = 10 - sum;
+		    }
+		    else
+		    {
+			count_X = count_X + 2;
+		    }
 
-            break;
-        }
-        case 'L':
-        {
-            count_L++;
-            if (max >= 50)
-            {
-                sum += 50;
-                max = 50;
-            }
-            else
-            {
-                sum = 50 - sum;
-            }
-            break;
-        }
-        case 'C':
-        {
-            count_C++;
-            if (max >= 100)
-            {
-                sum += 100;
-                max = 100;
-            }
-            else
-            {
-                sum = 100 - sum;
-            }
-            break;
-        }
-        case 'D':
-        {
-            count_D++;
-            if (max >= 500)
-            {
-                sum += 500;
-                max = 500;
-            }
-            else
-            {
-                sum = 500 - sum;
-            }
-            break;
-        }
-        case 'M':
-        {
-            if (max >= 1000)
-            {
-                sum += 1000;
-                max = 1000;
-            }
-            else
-            {
-                sum = 1000 - sum;
-            }
-            break;
-        }
-        default:
-        {
-            printf("\nInvalid entry\n");
-            break;
-        }
-    }
-	i++;
-    if (fun_input[i] !='\0')
-    {
-        function_check(fun_input);
-    }
-    else
-    {
-        if ((count_I <= max_no) && (count_C <= max_no) && (count_X <= max_no) && (count_V <= min_no) && (count_L <= min_no) && (count_D <= min_no))
-        {
-            printf("\nInterger is : %d\n", sum);
-        }
-        else
-        {
-            printf("\ninvalid input\n");
-        }
+		    break;
+		}
+		case 'L':
+		{
+		    count_L++;
+		    if (max >= 50)
+		    {
+			sum += 50;
+			max = 50;
+		    }
+		    else
+		    {
+			sum = 50 - sum;
+		    }
+		    break;
+		}
+		case 'C':
+		{
+		    count_C++;
+		    if (max >= 100)
+		    {
+			sum += 100;
+			max = 100;
+		    }
+		    else
+		    {
+			sum = 100 - sum;
+		    }
+		    break;
+		}
+		case 'D':
+		{
+		    count_D++;
+		    if (max >= 500)
+		    {
+			sum += 500;
+			max = 500;
+		    }
+		    else
+		    {
+			sum = 500 - sum;
+		    }
+		    break;
+		}
+		case 'M':
+		{
+		    if (max >= 1000)
+		    {
+			sum += 1000;
+			max = 1000;
+		    }
+		    else
+		    {
+			sum = 1000 - sum;
+		    }
+		    break;
+		}
+		default:
+		{
+		    printf("\nInvalid entry...\n");
+		    break;
+		}
+	}
+	
+	i++;							// Shifting Input char pointer 
+	
+	if (fun_input[i] !='\0')
+	{
+		function_check(fun_input);
+	}
+	else
+	{
+		if ((count_I <= max_no) && (count_C <= max_no) && (count_X <= max_no) && (count_V <= min_no) && (count_L <= min_no) && (count_D <= min_no))
+		{
+			printf("\nInterger is : %d\n", sum);
+		}
+		else
+		{
+			printf("Invalid input...\n");
+		}
+	}
 
-
-    }
-   
-    return 0;
+	return 0;
 }
 
